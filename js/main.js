cfbc675930e7a23c6795e3ccb5a64c3da72f429c
Mariacecilia1999 <conticecilia199@gamil.com>
2023-03-27 07:42:52 -0300

    Agrego clases para aplicarle estilos a la lista

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ let addTask = (e) =>{
    let valueTask = $('.inputTask').value
    console.log(valueTask)
    let divTasks = $('.showTasks')
-   divTasks.innerHTML += `<div>
-                           <h5>${valueTask}</h5>
-                           <button>X</button>
+   divTasks.innerHTML += `<div class="column is-flex is-justify-content-space-between is-align-content-center my-3">
+                              <h5>${valueTask}</h5>
+                              <button class='button has-background-danger-dark has-text-white-bis'>X</button>
                          </div>`
 } 
 
