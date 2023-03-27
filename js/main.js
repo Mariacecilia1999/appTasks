83236f92e2aaa0f5621f168dcf7d75202c88cc56
Mariacecilia1999 <conticecilia199@gamil.com>
2023-03-27 07:31:10 -0300

    Valido input y agrego tarea

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 let $ = selector => document.querySelector(selector)
 let $$ = selector => document.querySelectorAll(selector)
+
+
+let form = $('form')
+console.log(form)
+
+let addTask = (e) =>{
+   e.preventDefault()
+   let valueTask = $('.inputTask').value
+   console.log(valueTask)
+   let ulTasks = $('.showTasks')
+   ulTasks.innerHTML += `<ul>
+                           <li>${valueTask}</li>
+                         </ul>`
+} 
+
+$('.submit').addEventListener('click', addTask)
